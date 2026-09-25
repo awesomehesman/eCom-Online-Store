d64fa3e3db101637478ea7052a407cdaae8241fa
Luhle Hesman <luhle.hesman@ejgallo.com>
2026-09-25 20:12:54 +0200

    build(backend): establish executable Gradle baseline

---
+++ enterprise-fashion-ecommerce/backend/settings.gradle.kts
@@ -0,0 +1,16 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "backend"
